4b725dadd8d36f63b34d895348b03e6615f3804d
Matteo Chianale <matteo.chianale75@gmail.com>
2024-02-06 14:40:58 +0100

    script to build images

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 IMAGE_NAME_APP="app"
 if docker images --format "{{.Repository}}:{{.Tag}}" | grep -q "^${IMAGE_NAME_APP}$"; then
     docker rmi ${IMAGE_NAME_APP}
+fi
 docker build -t app -f Dockerfile.app .
 
 #DB
 IMAGE_NAME_DB="db"
 if docker images --format "{{.Repository}}:{{.Tag}}" | grep -q "^${IMAGE_NAME_DB}$"; then
     docker rmi ${IMAGE_NAME_DB}
+fi
 docker build -t db -f Dockerfile.db .
