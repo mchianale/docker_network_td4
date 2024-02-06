326e409417f2ffd3cfa4eea8c53a988e179f89e7
Matteo Chianale <matteo.chianale75@gmail.com>
2024-02-06 12:52:15 +0100

    fix issue

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,12 @@ import {Request, Response} from "express";
 const app = express();
 const port = 8080;
 
+// Get Liveliness
+app.get('', (req: Request, res: Response) => {
+    return res.status(200).send('API LIVE');
+});
+
+// Get health
 app.get('/health', (req: Request, res: Response) => {
     res.status(200).send('OK');
 });
