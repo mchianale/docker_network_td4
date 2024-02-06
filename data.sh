df51635fc602aa11d811fef512b409107c1281fc
Matteo Chianale <matteo.chianale75@gmail.com>
2024-02-06 16:29:27 +0100

    fix issue

--- data.sh
+++ data.sh
@@ -1,8 +1,8 @@
 #copy the dataset in the container db
-docker cp data/players.csv db:/data/players.csv
+docker cp data/players.csv db:/players.csv
 #create table
 docker exec -i db psql -U main -d my_db < data/create.sql
 #insert values
-docker exec -i db psql -U main -d my_db -c "\COPY player FROM '/data/players.csv' DELIMITER ';' CSV HEADER;"
+docker exec -i db psql -U main -d my_db -c "\COPY player FROM 'players.csv' DELIMITER ';' CSV HEADER;"
 
 
