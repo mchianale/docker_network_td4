29e6f504587d2f360572435770067a16d2418b08
Matteo Chianale <matteo.chianale75@gmail.com>
2024-02-06 14:32:09 +0100

    script to build images

--- build.sh
+++ build.sh
@@ -1,17 +1,2 @@
 docker build -t app -f Dockerfile.app .
-if [ $? -eq 0 ]; then
-    echo "API image ✓."
-else
-    echo "API image x."
-    exit 1
-fi
-
-
 docker build -t db -f Dockerfile.db .
-if [ $? -eq 0 ]; then
-    echo "DB image ✓"
-else
-    echo "DB image x"
-    exit 1
-fi
-
